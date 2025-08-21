264ad60d1400faa2a9c89b123c9f329f37f381f6
NotHalls74 <notharshith74@gmail.com>
2025-08-21 20:28:07 +0530

    the include was wrong and damn why was the clang format not applied to this?

--- TotallyReliableLibrary/Include/BinarySearch.h
+++ TotallyReliableLibrary/Include/BinarySearch.h
@@ -1,12 +1,13 @@
 #pragma once
 
-#include "Datatype.h"
+#include "DataType.h"
 
 /// @brief perform binary search algo on a sorted array of any datatype
 /// @param arr pass the address of the starting point of the array
 /// @param length pass length of the array
-/// @param target pass the address of the target that you want to search in the array 
+/// @param target pass the address of the target that you want to search in the
+/// array
 /// @param type pass the data type of the array
-/// @return it returns the index if found or -1 
+/// @return it returns the index if found or -1
 int TRL_BinarySearch(const void *arr, int length, const void *target,
                      TRL_DataType type);
