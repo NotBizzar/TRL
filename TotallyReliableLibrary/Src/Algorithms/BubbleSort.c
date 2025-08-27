83b656cba2a90ea92eb75a6788c304728abba78a
NotHalls <notharshith74@gmail.com>
2025-08-27 13:30:32 +0530

    some things need considaration

--- TotallyReliableLibrary/Src/Algorithms/BubbleSort.c
+++ TotallyReliableLibrary/Src/Algorithms/BubbleSort.c
@@ -9,7 +9,7 @@ void TRL_BubbleSort(void *arr, int len, TRL_DataType type)
   size_t dataType_size = TRL_DataTypeValue(type);
   for(int i = len - 1; i >= 0; i--)
   {
-    for(int j = 1; j <=i; j++)
+    for(int j = 1; j <= i; j++)
     {
       char *num1 = (char *)arr + ((j - 1) * dataType_size);
       char *num2 = (char *)arr + (j * dataType_size);
@@ -17,6 +17,8 @@ void TRL_BubbleSort(void *arr, int len, TRL_DataType type)
 
       if(cmp == 1)
       {
+        // dont really think this would be optimal
+        // i dont know for sure but someone needs to look into it
         void *temp = malloc(dataType_size);
         memcpy(temp, num2, dataType_size);
         memcpy(num2, num1, dataType_size);
